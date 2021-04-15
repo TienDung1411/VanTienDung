7746a3816f162f630c37ae892bed1ccc317b872d
TienDung1411 <81942704+TienDung1411@users.noreply.github.com>
2021-04-15 14:14:12 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,23 +3,20 @@ package org.o7planning.HibernateTutorial;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-
+import org.hibernate.service.ServiceRegistry;
 
 public class Test {
 public static void main(String[] args) {
 	Transaction transaction = null;
     try  {
     	Session session = HibernateUtils.getSessionFactory().openSession();
-        // start a transaction
         transaction = session.beginTransaction();
-        List < Staff > st = session.createQuery("from staff", Staff.class).list();
-
+        List < Staff > st = session.createQuery(" from Staff", Staff.class).list();
+        
         for (Staff emp : st) {
-            System.out.println(emp.getStaff_id()  );
-            System.out.println(emp.getStaff_name()  );
-            System.out.println(emp.getStaff_code()  );
-            System.out.println(emp.getBirth_day()  );
+            System.out.println(emp.getStaff_id() +"-"+ emp.getStaff_name()+
+            		"-"+emp.getStaff_code()+"-" + emp.getBirth_day());
+            
         }
         transaction.commit();
     } catch (Exception e) {
@@ -28,5 +25,7 @@ public static void main(String[] args) {
         }
         e.printStackTrace();
     }
+    
 }
-}	 
+
+}
