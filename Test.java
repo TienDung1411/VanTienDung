307953296ecec3df1d0fe6e610f55bf3b665c32e
TienDung1411 <81942704+TienDung1411@users.noreply.github.com>
2021-04-19 17:49:29 +0700

    Add files via upload
    spring-jdbc

--- Test.java
+++ Test.java
@@ -1,94 +1,22 @@
-package org.o7planning.HibernateTutorial;
-import java.util.Date;
-import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import org.hibernate.service.ServiceRegistry;
-
+package com.javatpoint;
+import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 public class Test {
-public static void main(String[] args) {
-	
-	Test.update(2, "123456789");
-	
-	Test.delete(2);
-	
-	Transaction transaction = null;
-    try  {
-    	Session session = HibernateUtils.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
-        Query < Staff > query = session.createQuery("from Staff", Staff.class);
-        List < Staff > st = query.list();
-        for (Staff emp : st) {
-            System.out.println(emp.getStaff_id() +"-"+ emp.getStaff_name()+
-            		"-"+emp.getStaff_code()+"-" + emp.getBirth_day());
-            
-        }
-        transaction.commit();
-    } catch (Exception e) {
-        if (transaction != null) {
-            transaction.rollback();
-        }
-        e.printStackTrace();
-    }
+	public static void main(String[] args) throws SQLException {
+	    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	    JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+	    
+//	    String sql = "INSERT INTO staff " + "VALUES (2, 'Nam', 'H122', '1999-07-27')";
+//	    jdbcTemplate.update(sql);
+	    
+//	    String sql = "UPDATE staff SET staff_name = 'Cuong' WHERE staff_id = 2";
+//	    jdbcTemplate.update(sql);
+	    
+//	    String sql = "delete from staff WHERE staff_id = 2";
+//	    jdbcTemplate.update(sql);
+   
 
-    
+	  }
 }
-
-// Hàm thêm dữ liệu
-public static void addstaff(long id, String name, String code, Date birth ) {
-	Transaction transaction = null;
-    try  {
-    	Session session = HibernateUtils.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
-        Staff newst = new Staff();
-        newst.setStaff_id(id);
-        newst.setStaff_name(name);
-        newst.setStaff_code(code);
-        newst.setBirth_day(birth);
-        session.saveOrUpdate(newst);
-        transaction.commit();
-    } catch (Exception e) {
-        if (transaction != null) {
-            transaction.rollback();
-        }
-        e.printStackTrace();
-    }
-}
-
-// Hàm thay đổi dữ liệu tại staff_id=?
-public static void update(long staff_id, String staff_code) {
-	Transaction transaction = null;
-    try  {
-    	Session session = HibernateUtils.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
-        Staff employee = (Staff) session.get(Staff.class, staff_id);
-        employee.setStaff_code(staff_code);
-        session.update(employee);
-        transaction.commit();
-    } catch (Exception e) {
-        if (transaction != null) {
-            transaction.rollback();
-        }
-        e.printStackTrace();
-    }
-}
-
-// Hàm xóa dữ liệu tại staff_id=?
-public static void delete(long staff_id) {
-	Transaction transaction = null;
-    try  {
-    	Session session = HibernateUtils.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
-        Staff employee = (Staff) session.get(Staff.class, staff_id);
-        session.delete(employee);
-        transaction.commit();
-    } catch (Exception e) {
-        if (transaction != null) {
-            transaction.rollback();
-        }
-        e.printStackTrace();
-    }
-}
-
-}	
